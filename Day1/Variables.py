402ee2e8d1970b9cb49ff926b0ae233fea8a70d9
Aditi Shinde <aaditishinde1804@gmail.com>
2024-04-07 21:04:09 +0530

    Amend_1

---
+++ Day1/Variables.py
@@ -0,0 +1,10 @@
+
+#a small code to understand variables in python converting celsius to farenheit
+
+
+celsius = float(input("Enter the temprature in celsius: "))
+
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("The temperature in Fahrenheit is: " , fahrenheit)
