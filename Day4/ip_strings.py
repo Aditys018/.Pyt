019ccf905ae2658f00debfb790b60bb79c9b466f
Aditi Shinde <aaditishinde1804@gmail.com>
2024-04-11 23:36:12 +0530

    amend4

---
+++ Day4/ip_strings.py
@@ -0,0 +1,6 @@
+# Two arrays are given arr1 and arr2. (Interview qu. sankeySolutions)
+# Find out which element of arr1 are not present in arr2
+#i/p      arr1:{1,2,3,4,5}
+#         arr2:{0,3,2,4}
+#o/p      {0,5}
+
