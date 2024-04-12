ee60ffcdba3d598aa86487973b73f78f2328e283
Aditi Shinde <aaditishinde1804@gmail.com>
2024-04-12 23:58:51 +0530

    Amend4

--- Day4/ip_strings.py
+++ Day4/ip_strings.py
@@ -1,8 +1,8 @@
 # Two arrays are given arr1 and arr2. (Interview qu. sankeySolutions)
 # Find out which element of arr1 are not present in arr2
-#i/p      arr1:{1,2,3,4,5}
+#  i/p      arr1:{1,2,3,4,5}
 #         arr2:{0,3,2,4}
-#o/p      {0,5}
+#  o/p      {0,5}
 
 
 arr1 = {1,2,3,4,5}
